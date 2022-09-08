56ab690b2d78b8bb790a3c03773ed08729cbc8f8
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-09-08 15:21:38 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+
+
+
 /**
-* Main print out the string in the function
-**/
-int main (void)
+
+* main - print the string in the put function
+
+*
+
+*Decription: using the main function
+
+* this program prints "ogramming is like building a multilingual puzzle"
+
+* Retun: 0
+
+*/
+
+int main(void)
+
 {
-	
-	puts("\"Programming is like building a multilingual puzzle");
+
+	puts("\"ogramming is like building a multilingual puzzle");
+
 	return (0);
+
 }
