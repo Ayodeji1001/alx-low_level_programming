4eed0b7716f742c6e44143663def390080dce344
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-09-09 07:42:16 -0700

    taks 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,12 @@
+
 #include <stdio.h>
-
 /**
-
-*main-print the string in the put function
-
+*main - print the string in the put function
 *
-
-*Description:using the main function
-
-*this program prints "programming is like building a multilingual puzzle
-
-*Return:0
-
-*/
-
-init main(void)
-
+* this program prints "Programming is like a multilingual puzzle"
+**/
+int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return(0);
-
 }
