e7b3a36ad51bc90fb1ae473f2506df0214a6c901
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-09-11 06:08:36 -0700

    Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,37 @@
+/*
+
+ * File: 4-puts.c
+
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+
+ */
+
+
 
 #include <stdio.h>
+
+
+
 /**
-*main - print the string in the put function
-*
-* this program prints "Programming is like a multilingual puzzle"
-**/
+
+ * main - Prints "Programming is like building a multilingual
+
+ *                puzzle, followed by a new line.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+
+	return (0);
+
 }
