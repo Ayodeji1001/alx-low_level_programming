2f85ec99e9c1ac7331f2d49f0d909f7128cc1e7f
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-09-12 10:59:53 -0700

    Task 102

--- 0x00-hello_world/102-print_comb5.c
+++ 0x00-hello_world/102-print_comb5.c
@@ -7,10 +7,10 @@ int main(void)
 {
 int num1, num2;
 for 
-(num1 = 0; num1 <= 98; num1++)
+(num1 = 0; num1 <= 98;num1++)
 {
 for
-(num2 = num1 + 1; num2 <= 99; num2++)
+(num2 = num1 + 1; num2 <= 99;num2++)
 {
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
@@ -21,7 +21,7 @@ if
 (num1 == 98 && num2 == 99)
 continue;
 putchar(',');
-putchar(',');
+putchar('i,');
 }
 }
 putchar('\n');
