297148a95ef222489c361cf4421387e12df2d4ea
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-09-13 10:57:40 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* _islower - Checks if a character is lowercase
-* Return: 1 if character is lowercase, 0 otherwise.
+* print_alphabet_x10 - a function that prints 10 times the alphabet
+* Return: x10 a-z
 */
 void print_alphabet_x10(void)
 {
