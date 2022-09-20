761717641ad7154515073fdbf0e75c6ab4bc9dfb
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-09-20 08:17:54 -0700

    Task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,22 @@
 #include "main.h"
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+char c;
+int x;
+srand(time(0));
+while (x <= 2645)
+{
+c = rand() % 128;
+x += c;
+putchar(c);
+}
+putchar(2772 - x);
 return (0);
 }
