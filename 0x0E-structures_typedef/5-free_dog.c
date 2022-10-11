459af5393d2fedbbeabbb71551f502c5d2c2c246
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-10-11 07:07:09 -0700

    Task 4

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,12 +3,15 @@
 #include "dog.h"
 /**
  * free_dog - Entry Point
- * @d: struct to free
- * Return: 0
+ * @d: pointer to struct
+ * Return: what does it return?
  */
 void free_dog(dog_t *d)
 {
-if (d == NULL)
-return;
+if (d)
+{
+free(d->owner);
+free(d->name);
 free(d);
+}
 }
