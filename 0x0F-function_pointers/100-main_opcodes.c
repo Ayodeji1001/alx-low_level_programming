7d40381f0c81bee95f38434f355997ec7bced2d0
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-10-12 07:51:35 -0700

    Task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+/**
+ * main - define function
+ * @void - describe argument
+ * Return: what does it return?
+ */
+int main(void)
+{
+return (0);
+}
