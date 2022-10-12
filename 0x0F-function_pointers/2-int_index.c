737c46f88deaffbe8bc44af74a383b657089aa9a
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-10-12 07:43:02 -0700

    Task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,13 +1,26 @@
-#include <unistd.h>
-
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
  */
-int _putchar(char c)
+int int_index(int *array, int size, int (*cmp)(int))
 {
-return (write(1, &c, 1));
+int i;
+if (array == NULL)
+return (-1);
+if (size <= 0)
+return (-1);
+if (cmp == NULL)
+return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
 }
