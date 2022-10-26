8b8b0d0389b53cdb69145d48c7de9fad74ac9492
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-10-25 17:16:50 -0700

    Task correction

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,31 +8,28 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 {
-unsigned int count = 0;
-listint_t *copy;
-listint_t *temp;
-if (*head == NULL)
-return (-1);
-copy = *head;
-if (index == 0)
+listint_t *index_node, *tmp_node = *head;
+unsigned int i = 0;
+if (tmp_node && (index == 0))
 {
-temp = copy->next;
-free(copy);
-*head = temp;
+*head = tmp_node->next;
+free(tmp_node);
 return (1);
 }
-while (copy != NULL)
+while (tmp_node)
 {
-if (count + 1 == index)
+if (i + 1 == index)
 {
-temp = copy->next;
-copy->next = temp->next;
-free(temp);
+index_node = tmp_node->next;
+if (index_node)
+{
+tmp_node->next = index_node->next;
+free(index_node);
 return (1);
 }
-if (copy->next != NULL)
-copy = copy->next;
-count++;
+}
+tmp_node = tmp_node->next;
+i++;
 }
 return (-1);
 }
