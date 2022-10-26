2ead9750c813933bc97ac2b95b3ddda4408700ac
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-10-25 17:21:11 -0700

    Correction

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,7 +7,19 @@
 size_t print_listint_safe(const listint_t *head)
 
 {
+size_t count = 0;
 if (head == NULL)
-exit(98);
 return (0);
+while (head != NULL)
+{
+count++;
+printf("[%p] %d\n", (void *)head, head->n);
+if (head->next >= head)
+{
+printf("-> [%p] %d\n", (void *)head->next, (head->next)->n);
+break;
+}
+head = head->next;
+}
+return (count);
 }
