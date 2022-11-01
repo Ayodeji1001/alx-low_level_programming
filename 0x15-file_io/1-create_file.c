cda7fd6e18067a78bcf3186fd000460a3b64c9db
Ayodeji1001 <ayodeji.suleiman23@gmail.com>
2022-11-01 08:27:41 -0700

    Task updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,30 +1,28 @@
 #include "main.h"
 /**
- * create_file - Entry Point
- * @filename: file name
- * @text_content: null terminated string to write
- * Return: 1
+ * read_textfile - Reads a textfile and prints the contents to the POSIX STDOUT
+ * @filename: The name of the file to read from
+ * @letters: The number of characters it should print to the STDOUT
+ * Return: Returns the number of characters printed
  */
-int create_file(const char *filename, char *text_content)
+ssize_t read_textfile(const char *filename, size_t letters)
 
 {
-int file, i = 0;
-if (filename == NULL)
-return (-1);
-file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-if (file == -1)
-return (-1);
-while (text_content[i])
-i++;
-if (text_content == NULL)
-{
-close(file);
-return (-1);
-}
-else
-{
-write(file, text_content, i);
-}
-close(file);
-return (1);
+ssize_t fild = 0, chk = 0;
+char *buffer;
+if (!filename || !letters)
+return (0);
+fild = open(filename, O_RDONLY);
+if (fild < 0)
+return (0);
+buffer = malloc(sizeof(char) * (letters));
+if (!buffer)
+return (0);
+chk = read(fild, buffer, letters);
+chk = write(STDOUT_FILENO, buffer, chk);
+if (chk < 0)
+return (0);
+close(fild);
+free(buffer);
+return (chk);
 }
